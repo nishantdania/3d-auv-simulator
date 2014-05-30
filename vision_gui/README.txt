8312a17c09fde3d8c29dcbee9f277653de140187
huixian <lynnetteng2002@gmail.com>
2014-05-30 16:39:10 +0800

    Added CAD files and vision_gui

---
+++ vision_gui/README.txt
@@ -0,0 +1,8 @@
+Was originally a ROS node to be run with command: rosrun gui vision_gui_node
+
+Has front and bottom camera input image and filter image
+
+image topics:
+/front_camera/camera/image_raw
+/bot_camera/camera/image_raw
+
---
+++ vision_gui/vision_gui.h
@@ -0,0 +1,8 @@
+/****************************************************************************
+** Form interface generated from reading ui file 'vision.ui'
+**
+** Created: Fri May 30 16:20:30 2014
+**
+** WARNING! All changes made in this file will be lost!
+****************************************************************************/
+
