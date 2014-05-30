f635d9a6906c17fa4e948f8902e49c1a40f0c21d
nishantdania <nishantdania@gmail.com>
2014-05-31 00:32:21 +0530

    Added simulator files

---
+++ simulator/pandaManager/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+project:		3d-auv-simulator
+author:			nishant dania
+email: 			nishantdania@gmail.com
+modified on:	May 26, 2014
+
+"""
+
+from PandaScene import PandaScene
+from ModelNode import ModelNode
+from SceneGraphManager import SceneGraphManager
