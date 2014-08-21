523ddb29b8283ec911b11f6dd20200ccaf9d127e
lynnette <lynnette@lynnette.local>
2014-08-21 09:36:38 +0800

    Removed unnecessary files

--- vision_gui/vision_gui.h
+++
@@ -1,8 +0,0 @@
-/****************************************************************************
-** Form interface generated from reading ui file 'vision.ui'
-**
-** Created: Fri May 30 16:20:30 2014
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
-
